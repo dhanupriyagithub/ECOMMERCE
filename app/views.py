cf9223449771f829aad612f36cc1e6b7a72ca826
dhanupriyagithub <137754613+dhanupriyagithub@users.noreply.github.com>
2023-11-29 20:10:50 +0530

    Add files via upload

---
+++ app/views.py
@@ -0,0 +1,100 @@
+from django.db.models import Count
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from django.views import View
+from . models import Customer, product
+from . forms import CustomerRegistrationForm,CustomerProfileForm
+from django.contrib import messages
+
+def home(request):
+    return render(request,"app/home.html")
+
+def about(request):
+    return render(request,"app/about.html")
+
+def contact(request):
+    return render(request,"app/contact.html")
+
+class CategoryView(View):
+    def get(self,request,val):
+        Product = product.objects.filter(category=val)
+        title = product.objects.filter(category=val).values('title')
+        return render(request,"app/category.html",locals())
+
+class CategoryTitle(View):
+    def get(self,request,val):
+        Product = product.objects.filter(title=val)
+        title = product.objects.filter(category=Product[0].category).values('title')
+        return render(request,"app/category.html",locals())
+
+
+class ProductDetail(View):
+    def get(self,request,pk):
+        Product = product.objects.get(pk=pk)
+        return render(request,"app/productdetail.html",locals())
+
+
+class CustomerRegistrationView(View):
+    def get(self,request):
+        form = CustomerRegistrationForm()
+        return render(request,"app/customerregistration.html",locals())
+    
+    def post(self,request):
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Congratulations! User Register Successfully")
+        else:
+            messages.warning(request,"Invalid Input Data")
+        return render(request,"app/customerregistration.html",locals())
+    
+
+class ProfileView(View):
+    def get(self,request):
+        form = CustomerProfileForm()
+        return render(request,"app/profile.html",locals())
+    def post(self,request):
+        form = CustomerProfileForm(request.POST)
+        if form.is_valid():
+            user = request.user
+            name = form.cleaned_data['name']
+            locality = form.cleaned_data['locality']
+            city = form.cleaned_data['city']
+            mobile = form.cleaned_data['mobile']
+            state = form.cleaned_data['state']
+            zipcode = form.cleaned_data['zipcode']
+
+            reg = Customer(user=user,name=name,locality=locality,mobile=mobile,city=city,state=state,zipcode=zipcode)
+            reg.save()
+            messages.success(request,"Congratulations! Profile Save Successfully")
+        else:
+            messages.warning(request,"Invalid Input Data")
+        return render(request,"app/profile.html",locals())
+    
+def address(request):
+    add = Customer.objects.filter(user=request.user)
+    return render(request,'app/address.html',locals())
+
+class UpdateAddress(View):
+    def get(self,request,pk):
+        add = Customer.objects.get(pk=pk)
+        form = CustomerProfileForm(instance=add)
+        return render(request,'app/updateAddress.html',locals())
+
+    def post(self,request,pk):
+        form = CustomerProfileForm(request.POST)
+        if form.is_valid():
+            add = Customer.objects.get(pk=pk)
+            add.user = request.user
+            add.name = form.cleaned_data['name']
+            add.locality = form.cleaned_data['locality']
+            add.city = form.cleaned_data['city']
+            add.mobile = form.cleaned_data['mobile']
+            add.state = form.cleaned_data['state']
+            add.zipcode = form.cleaned_data['zipcode']
+            add.save()
+            messages.success(request,"Congratulations! Profile Save Successfully")
+        else:
+            messages.warning(request,"Invalid Input Data")
+        return redirect("address")
+
